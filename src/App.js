245b998a4f7b50d86b2407ebb84f1fba2865a810
strawberrycat18 <scewcc@hotmail.com>
2023-03-29 21:52:33 +0800

    Added CRUD features

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import LoginPage from "./views/LoginPage"
+import PostPageAdd from './views/PostPageAdd';
+import PostPageDetails from './views/PostPageDetails';
 import PostPageHome from './views/PostPageHome';
+import PostPageUpdate from './views/PostPageUpdate';
 import SignUpPage from './views/SignUpPage';
 
 function App() {
@@ -11,6 +14,10 @@ function App() {
           <Route path="/" element={<PostPageHome/>}/>
           <Route path="/login" element={<LoginPage/>}/>
           <Route path="/signup" element={<SignUpPage/>}/>
+          <Route path="/add" element={<PostPageAdd/>}/>
+          <Route path="/post/:id" element={<PostPageDetails/>}/>
+          <Route path="/update/:id" element={<PostPageUpdate/>}/>
+
       </Routes>
     </BrowserRouter>
   );
